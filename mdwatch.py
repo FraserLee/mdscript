7ad9273618829f1b1d9bcae3f4f14479e81e7d8f
Unknown <tomatoesneverlie@gmail.com>
2022-01-07 12:35:07 -0500

    added way to call rust or python version from watcher script
    make it usable while I rewrite things. Also added live reload to python version via the -d flag.

--- mdwatch.py
+++ mdwatch.py
@@ -6,33 +6,55 @@ import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+from importlib import reload
+
 import mdscript
 
 last_compile_time = -1
 file_path = ''
 dest_path = ''
 
+rust_mode = False
+debug_mode = False
+
 class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
+        # super simple debounce
         global last_compile_time
-        if time.time() - last_compile_time < 0.5: # super simple debounce
+        if time.time() - last_compile_time < 0.5:
             return
         last_compile_time = time.time()
+
         print('recompiling...', end='')
-        mdscript.compile_file(file_path, dest_path)
+        if rust_mode: # either call cargo or the pre-compiled binary
+            if debug_mode: os.system(f'cargo run {file_path} {dest_path}')
+            else: os.system(f'target/debug/mdscript {file_path} {dest_path}')
+        else:
+            if debug_mode: reload(mdscript) # reload if in debug mode
+            mdscript.compile_file(file_path, dest_path)
         print('done')
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python3 mdwatch.py <path>')
-        sys.exit(1)
+    if '-r' in sys.argv[1:3] or '--rust' in sys.argv[1:3]:
+        rust_mode = True
+    if '-d' in sys.argv[1:3] or '--debug' in sys.argv[1:3]:
+        debug_mode = True
+    if len(sys.argv) != 2 + rust_mode + debug_mode:
+        print('usage: mdwatch.py [--rust | -r] [--debug | -d] <file_path>')
+        exit(1)
 
     # directory of the file to watch
-    file_path = os.path.abspath(sys.argv[1])
+    file_path = os.path.abspath(sys.argv[-1])
     dir_path = os.path.dirname(file_path)
     dest_path = dir_path + '/' + os.path.basename(file_path).split('.')[0] + '.html'
     print(f'Watching {dir_path}\n- press ctrl+c to exit\n')
+
+    # if we're in rust mode, cd to the directory of this script to run cargo
+    if rust_mode:
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        # if not debug_mode, build it once in advance
+        if not debug_mode: os.system('cargo build --release')
 
     # recompile any time anything changes in the directory
     event_handler = Handler()
@@ -42,9 +64,10 @@ if __name__ == '__main__':
     observer.start()
 
     # DEBUG: while in development, run a second observer to watch the directory of the build stuff
-    observer2 = Observer()
-    observer2.schedule(event_handler, os.path.dirname(os.path.abspath(sys.argv[0])), recursive=True)
-    observer2.start()
+    if debug_mode:
+        observer2 = Observer()
+        observer2.schedule(event_handler, os.path.dirname(os.path.abspath(sys.argv[0])), recursive=True)
+        observer2.start()
     # END DEBUG
 
     try:
