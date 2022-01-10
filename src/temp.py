3207679d21ff7f5edbbb18994a24f9a928a49bb2
Unknown <tomatoesneverlie@gmail.com>
2022-01-10 12:06:39 -0500

    removed a lot of the tempfile

--- src/temp.py
+++ src/temp.py
@@ -1,31 +1,3 @@
-# <CLI INVOCATION>
-if __name__ == '__main__':
-    if len(sys.argv) == 1:   # no arguments -> unittest
-        unittest.main()
-        sys.exit(0)
-    elif len(sys.argv) == 2: # output to stdout
-        compile_file(sys.argv[1])
-        sys.exit(0)
-    elif len(sys.argv) == 3: # output to file
-        compile_file(sys.argv[1], sys.argv[2])
-        sys.exit(0)
-    else:
-        print("Usage: mdscript.py [file] [destination (optional)]")
-        sys.exit(1)
-# </CLI INVOCATION>
-
-def compile_file(src, dest = None):
-    with open(src, 'r') as src:
-        result = compile_lines(src.readlines())
-        if dest:
-            with open(dest, 'w') as dest:
-                dest.write(result)
-        else:
-            print(result)
-
-import re
-from dataclasses import dataclass, field
-
 @dataclass
 class paragraph:
     indent: int
@@ -145,19 +117,6 @@ def parse_lines(lines):
             result.append(empty())
             continue
         # <EMPTY LINES>
-        # <HEADING>
-        match = re.match(r'^(#+) ([^\n]+)\n', line)
-        if match:
-            level = len(match.group(1))
-            level = min(level, 6)
-            result.append(h(level, parse_text(match.group(2))))
-            continue
-        # </HEADING>
-        # <HR>
-        if line.strip() == '---':
-            result.append(hr())
-            continue
-        # </HR>
         # <IMG>
         match = re.match(r'^!\[(.+?)\]\((.+?)\)', line)
         if match:
@@ -249,154 +208,12 @@ class linelist:
             return None
 
 def parse_text(text):
-    """
-    Clean text from markdown.
-    """
-    # replace characters with html equivalents
-    text = text.replace('&', '&amp;')
-    text = text.replace('<', '&lt;')
-    text = text.replace('>', '&gt;')
-    text = text.replace('"', '&quot;')
-    text = text.replace("'", '&#39;')
-
-    # temporarily replace escaped characters markers
-    text = text.replace(r'\*', '!ASTERIX_COMPILE_TIME_ESCAPE!')
-    text = text.replace(r'\_', '!UNDERSCORE_COMPILE_TIME_ESCAPE!')
-    text = text.replace(r'\~', '!TILDE_COMPILE_TIME_ESCAPE!')
-    text = text.replace(r'\`', '!BACKTICK_COMPILE_TIME_ESCAPE!')
-    text = text.replace(r'\$', '!DOLLAR_COMPILE_TIME_ESCAPE!')
-    text = text.replace(r'\%', '!PERCENT_COMPILE_TIME_ESCAPE!')
-
-    # comments from percent to end of line
-    text = re.sub(r'%.*', '', text)
-
-    # italics, bold, strikethrough, inline code
-    text = re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', text)
-    text = re.sub(r'\*(.+?)\*', r'<i>\1</i>', text)
-    text = re.sub(r'~~(.+?)~~', r'<s>\1</s>', text)
-    text = re.sub(r'`(.+?)`', r'<code>\1</code>', text)
-
     # line-mode latex math (processed by mathjax, very wip) 
     text = re.sub(r'\$\$(.+?)\$\$', r'\[\1\]', text)
 
-    # inline latex math (I'll compute this all at compile time later)
-    text = re.sub(r'\$(.+?)\$', r'\(\1\)', text)
 
-
-    # replace escaped characters markers
-    text = text.replace('!ASTERIX_COMPILE_TIME_ESCAPE!', '*')
-    text = text.replace('!UNDERSCORE_COMPILE_TIME_ESCAPE!', '_')
-    text = text.replace('!TILDE_COMPILE_TIME_ESCAPE!', '~')
-    text = text.replace('!BACKTICK_COMPILE_TIME_ESCAPE!', '`')
-    text = text.replace('!DOLLAR_COMPILE_TIME_ESCAPE!', '$')
-    text = text.replace('!PERCENT_COMPILE_TIME_ESCAPE!', '%')
-
-    # links
-    text = re.sub(r'\[(.+?)\]\((.+?)\)', r'<a href="\2">\1</a>', text)
-
-    return text.strip()
 
 def header(t_col, b_col):
-    result = r'''<!DOCTYPE html>
-<html>
-    <head>
-    <meta charset="utf-8">
-    <title>Markdown</title>
-    <style>
-       body {
-            font-family: sans-serif;
-            padding: 4em 0;
-            margin: 0;
-            width: 100vw;
-            font-size: 16px;
-            line-height: 1.5;
-            overflow-x: hidden;
-
-            --dd: #07080C;
-            --d0: #30353D;
-            --d1: #2E3440;
-            --d2: #3B4252;
-            --d3: #434C5E;
-            --d4: #4C566A;
-
-            --ll: #FAFAFA;
-            --l0: #F0F0F0;
-            --l1: #ECEFF4;
-            --l2: #E5E9F0;
-            --l3: #D8DEE9;
-
-            --f1: #5E81AC;
-            --f2: #81A1C1;
-            --f3: #88C0D0;
-            --f4: #8FBCBB;
-
-            --a1: #BF616A;
-            --a2: #D08770;
-            --a3: #EBCB8B;
-            --a4: #A3BE8C;
-            --a5: #B47EAD;
-
-            '''
     result += f'background-color: var(--{b_col});\n            color: var(--{t_col});'
     result += r'''
-        }
-        .outerbox {
-            margin: 0;
-            width: 100vw;
-        }
-        .innerbox {
-            margin: auto;
-            padding: 0.5em 4em;
-            max-width: 48em;
-        }
-
-        h1 {
-            font-weight: 100;
-            font-size: 5em;
-            margin-bottom: 0.5em;
-            text-align: center;
-        }
-        h2 {
-            font-weight: 200;
-            font-size: 3.5em;
-            margin: 0.5em 0;
-        }
-        h3 {
-            font-weight: 500;
-            font-size: 1.5em;
-            margin: 0.5em 0;
-        }
-        img {
-            width: 100%;
-        }
-        code {
-            font-family: monospace;
-            font-size: 1em;
-            padding: 0.1em 0.2em;
-            border-radius: 0.2em;
-            background-color: var(--d4);
-            color: var(--l0);
-        }
-        a:link {
-            color: var(--f2);
-        }
-
-        a:visited {
-            color: var(--f1);
-        }
-    </style>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"></script>
-</head>
-<body>
-<div class="outerbox">
-<div class="innerbox">
-'''
-
-    return result
-
-import unittest
-class Test(unittest.TestCase):
-    def test_compile_paragraphs(self):
-        self.assertEqual(compile_md("Paragraph"), "<p>Paragraph</p>\n")
 
